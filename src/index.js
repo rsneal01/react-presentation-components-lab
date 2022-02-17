b27de77a0c96d7041a89da689e042375376092cb
rsneal01 <rsneal01@gmail.com>
2022-02-17 08:32:16 -0800

    commit tree da1f5a381ac06fe603b839190cebac7e075c8f3c

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert("i am clicked")} />
   </div>,
   document.getElementById('root')
 );
